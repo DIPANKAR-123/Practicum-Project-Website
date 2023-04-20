7b6bac98688657c8bbffe0fa4f9bed35e754ba81
Dipankar <dipankaryadav1234@gmail.com>
2023-04-20 15:50:25 +0530

    updated

---
+++ google-auth2/client/src/pages/Home/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "Students",
+    title: "Students",
+  },
+  {
+    id: "signup",
+    title: "SignUp",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Get Exam Analytics",
+    content:
+      "Analyse strengths, weaknesses, wasted time and much more with data driven test results that help you improve faster.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Personalised Dashboard",
+    content:
+      "Track your progress and detailed test performance metrics at one place.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "EdNext Community",
+    content:
+      "An interactive student-teacher community to ask and answer questions, initiate discussions and share ideas",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "EdNext has plenty of questions.Doing them makes constructive use of your time. The interface which EdNext has, it’s really intuitive.",
+    name: "Manish Kumar",
+    title: "IIT Delhi",
+    // img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "I used to do concept tests on EdNext. They were quite beneficial. They are good for enhancing concept and practicing problems.",
+    name: "Ishaan Singh",
+    title: "IIIT Hyderabad",
+    // img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      " After reading or studying any concept, I used to solve concept tests on EdNext. That helped me to analyse what’s the depth to I've reached in that particular concept.",
+    name: "Himanshu Singh",
+    title: "IIT Bombay",
+    // img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Active Learners",
+    value: "800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Schools and Colleges",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Hours Spent",
+    value: "$4000+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/dipankar.yadav.10/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://twitter.com/DipankarYadav10",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/dipankar-yadav-ba6663157/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
